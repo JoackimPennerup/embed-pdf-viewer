754da2fe22d10feeedd4d0886d322173d563a8c6
Joackim Pennerup <joackim.pennerup@gmail.com>
2025-09-04 14:32:33 +0200

    chore: record changeset for adapters

--- packages/plugin-a11y/src/preact/adapter.ts
+++ packages/plugin-a11y/src/preact/adapter.ts
@@ -1,5 +1,5 @@
 export { Fragment } from 'preact';
-export { useEffect, useRef, useState } from 'preact/hooks';
+export { useEffect, useMemo, useRef, useState } from 'preact/hooks';
 export type { ComponentChildren as ReactNode } from 'preact';
 
 export type CSSProperties = import('preact').JSX.CSSProperties;
